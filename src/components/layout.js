ae63907aed6982094ffaf444962c4cdee51d9830
Joe Previte <jjprevite@gmail.com>
2019-05-07 14:11:40 -0700

    Fix large font-awesome icons on initial load

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,10 @@ import { StaticQuery, graphql } from 'gatsby'
 import Header from './atoms/Header'
 import Footer from './atoms/Footer'
 import GlobalStyle from '../utils/globalStyles'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+// Prevent fontawesome from dynamically adding its css since we did it manually above
+config.autoAddCss = false
 
 const Layout = ({ children }) => (
   <StaticQuery
