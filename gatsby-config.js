d9fc21bafabcc4f1023a7a209ce4f08bd317b59f
Joe Previte <jjprevite@gmail.com>
2019-05-07 13:52:23 -0700

    Update icons and gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,13 +46,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `JavaScript Joe`,
+        short_name: `jsjoeio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#F4E11E`,
+        theme_color: `#F4E11E`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/jsjoeio-icon.png` // This path is relative to the root of the site.
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
