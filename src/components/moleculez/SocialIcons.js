94c441502f3fa23c829e88d142cbd5491262e00d
JavaScript Joe <jjprevite@gmail.com>
2019-07-06 10:46:20 -0700

    Add icon and link for newsletter

--- src/components/moleculez/SocialIcons.js
+++ src/components/moleculez/SocialIcons.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import { faTwitter, faGithub, faDev } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons'
 import Icon from '../atoms/Icon'
 
 const socialSites = {
@@ -32,10 +33,10 @@ const SocialIcons = () => {
   const { twitter, github, devto } = data.site.siteMetadata.social
   return (
     <div>
-      <Icon icon={faTwitter} url={`${socialSites.twitter.url}${twitter}`} alt="Link to javascript joe's twitter" />
-      <Icon icon={faGithub} url={`${socialSites.github.url}${github}`} alt="Link to javascript joe's github" />
-      <Icon icon={faDev} url={`${socialSites.devto.url}${devto}`} alt="Link to javascript joe's dev.to profile" />
-      <Icon icon={faEnvelope} url={`${socialSites.devto.url}${devto}`} alt="Link to javascript joe's dev.to profile" />
+      <Icon icon={faTwitter} url={`${socialSites.twitter.url}${twitter}`} alt="Link to javascript joe's twitter." />
+      <Icon icon={faGithub} url={`${socialSites.github.url}${github}`} alt="Link to javascript joe's github." />
+      <Icon icon={faDev} url={`${socialSites.devto.url}${devto}`} alt="Link to javascript joe's dev.to profile." />
+      <Icon icon={faEnvelopeOpenText} url={`https://buttondown.email/jsjoeio`} alt="Link to subscribe to javascript joe's newsletter." />
     </div>
   )
 }
